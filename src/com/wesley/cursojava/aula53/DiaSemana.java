20da1d7ff76e592ae67ab998dd8277fb88da3431
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-07-06 19:56:57 -0300

    Aula 53

---
+++ src/com/wesley/cursojava/aula53/DiaSemana.java
@@ -0,0 +1,6 @@
+package com.wesley.cursojava.aula53;
+
+public enum DiaSemana {
+
+    SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+}
---
+++ src/com/wesley/cursojava/aula53/Teste.java
@@ -0,0 +1,75 @@
+package com.wesley.cursojava.aula53;
+
+import com.sun.webkit.dom.CSSImportRuleImpl;
+
+public class Teste {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        usandoCosntantes();
+
+        System.out.println();
+
+        usandoEnum();
+    }
+
+    private static void usandoCosntantes(){
+        int segunda = DiaSemanaConstantes.SEGUNDA;
+        int terca = DiaSemanaConstantes.TERCA;
+        int quarta = DiaSemanaConstantes.QUARTA;
+        int quinta = DiaSemanaConstantes.QUINTA;
+        int sexta = DiaSemanaConstantes.SEXTA;
+        int sabado = DiaSemanaConstantes.SABADO;
+        int domingo = DiaSemanaConstantes.DOMINGO;
+
+        System.out.println("Teste utilizando constantes no Java");
+        imprimeDiaSemana(segunda);
+        imprimeDiaSemana(terca);
+        imprimeDiaSemana(quarta);
+        imprimeDiaSemana(quinta);
+        imprimeDiaSemana(sexta);
+        imprimeDiaSemana(sabado);
+        imprimeDiaSemana(domingo);
+
+    }
+
+    private static void usandoEnum(){
+
+        DiaSemana segunda = DiaSemana.SEGUNDA;
+        DiaSemana terca = DiaSemana.TERCA;
+        DiaSemana quarta = DiaSemana.QUARTA;
+        DiaSemana quinta = DiaSemana.QUINTA;
+        DiaSemana sexta = DiaSemana.SEXTA;
+        DiaSemana sabado = DiaSemana.SABADO;
+        DiaSemana domingo = DiaSemana.DOMINGO;
+
+        System.out.println("Teste utilizando enum no Java");
+    }
+
+    private static void imprimeDiaSemana(DiaSemana dia){
+        switch (dia){
+            case SEGUNDA:
+                System.out.println("Segunda-Feira");
+                break;
+            case TERCA:
+                System.out.println("Terça-Feira");
+                break;
+            case QUARTA:
+                System.out.println("Quarta-Feira");
+                break;
+            case QUINTA:
+                System.out.println("Quinta-Feira");
+                break;
+            case SEXTA:
+                System.out.println("Sexta-Feira");
+                break;
+            case SABADO:
+                System.out.println("Sábado");
+                break;
+            case DOMINGO:
+                System.out.println("Domingo");
+                break;
+        }
+    }
+}
