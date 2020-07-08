0eef9e0b20ac4fe9856f4f586c4d6e2c7c414146
Wesley Menezes Januário <wesleyssjanuario@gmail.com>
2020-07-08 12:16:00 -0300

    Aula 53

--- src/com/wesley/cursojava/aula53/Teste.java
+++ src/com/wesley/cursojava/aula53/Teste.java
@@ -72,4 +72,30 @@ public class Teste {
                 break;
         }
     }
+
+    private static void imprimeDiaSemana(int dia){
+        switch (dia){
+            case 1:
+                System.out.println("Segunda-Feira");
+                break;
+            case 2:
+                System.out.println("Terça-Feira");
+                break;
+            case 3:
+                System.out.println("Quarta-Feira");
+                break;
+            case 4:
+                System.out.println("Quinta-Feira");
+                break;
+            case 5:
+                System.out.println("Sexta-Feira");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+        }
+    }
 }
